7cbb386473c97277a462bf4d984f126be15f923e
Mupingz <124030244+Mupingz@users.noreply.github.com>
2023-02-01 16:19:56 +0700

    Update selectionSort.ts

--- selectionSort.ts
+++ selectionSort.ts
@@ -1,18 +1,20 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 console.log(`original: ${numbers}`)
 
-function bubleSort(arr: number[]) { // O(n^2)
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let hold = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = hold
+function selectionSort(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i
+        let holding = arr[i]
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j
             }
         }
+        arr[i] = arr[minIndex]
+        arr[minIndex] = holding
     }
 }
+selectionSort(numbers)
+console.log(`selectionSort: ${numbers}`)
 
-bubleSort(numbers)
-console.log(`bubleSort: ${numbers}`)
 
