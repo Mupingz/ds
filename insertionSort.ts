c17fa2650589b33f8b61edf7f4a19643c4332d67
Mupingz <124030244+Mupingz@users.noreply.github.com>
2023-02-01 16:16:42 +0700

    Update insertionSort.ts

--- insertionSort.ts
+++ insertionSort.ts
@@ -1,18 +1,23 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 console.log(`original: ${numbers}`)
 
-function bubleSort(arr: number[]) { // O(n^2)
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let hold = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = hold
+function insertionSort(arr: number[]) { //O(n^2)
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[0]) {
+            const holding = arr.splice(i, 1)
+            arr.unshift(...holding)
+        } else {
+            if (arr[i] < arr[i - 1]) {
+                for (let j = 1; j < i; j++) {
+                    if (arr[i] >= arr[j - 1] && arr[i] < arr[j]) {
+                        arr.splice(j, 0, ...arr.splice(i, 1))
+                    }
+                }
             }
         }
     }
 }
 
-bubleSort(numbers)
-console.log(`bubleSort: ${numbers}`)
+insertionSort(numbers)
+console.log(`selectionSort: ${numbers}`)
 
